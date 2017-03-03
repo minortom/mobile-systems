2a9cbfc8caececfed3bd37d4eb47afd2c88c0f5e
Tom Arnoldussen <tom.arnoldussen@gmail.com>
2017-03-03 13:16:47 +0100

    login for all other services comment added

--- www/app/controllers/loginController.js
+++ www/app/controllers/loginController.js
@@ -9,5 +9,19 @@ app.controller('loginController', ['$scope','processApiCallService', 'apiService
 		 $location.url('/tab/overview');
 		 });
 	};
-	
+
+	// http://ngcordova.com/docs/plugins/oauth/
+	 // $cordovaOauth.dropbox(string appKey);
+    // $cordovaOauth.digitalOcean(string clientId, string clientSecret);
+    // $cordovaOauth.google(string clientId, array appScope);
+    // $cordovaOauth.github(string clientId, string clientSecret, array appScope);
+    // $cordovaOauth.linkedin(string clientId, string clientSecret, array appScope, string state);
+    // $cordovaOauth.instagram(string clientId, array appScope);
+    // $cordovaOauth.box(string clientId, string clientSecret, string state);
+    // $cordovaOauth.reddit(string clientId, string clientSecret, array appScope);
+    // $cordovaOauth.twitter(string consumerKey, string consumerSecretKey);
+    // $cordovaOauth.meetup(string consumerKey);
+    // $cordovaOauth.foursquare(string clientId);
+    // $cordovaOauth.salesforce(string loginUrl, string clientId);
+    // $cordovaOauth.strava(string clientId, string clientSecret, array appScope);
 }]);
