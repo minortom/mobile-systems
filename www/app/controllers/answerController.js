88baf4ef8da04e6f27e414e80efd7f6d69b953b2
Diederik Beker <diederik@stuvia.com>
2017-03-14 19:00:32 +0100

    Level info and answer tab

--- www/app/controllers/answerController.js
+++ www/app/controllers/answerController.js
@@ -1,4 +1,8 @@
-app.controller('answerController', ['$scope','processApiCallService', 'apiService','$state','store', '$rootScope', 'authService', '$location', '$fancyModal','$sce', '$timeout','$cordovaOauth', function ($scope, processApiCallService, apiService, $state, store, $rootScope, authService, $location, $fancyModal, $sce, $timeout, $cordovaOauth) {
-	
-	
+app.controller('answerController', ['$scope','processApiCallService', 'apiService','$state','store', '$rootScope', 'authService', '$location', '$fancyModal','$sce', '$timeout','$cordovaOauth', '$ionicPopup',function ($scope, processApiCallService, apiService, $state, store, $rootScope, authService, $location, $fancyModal, $sce, $timeout, $cordovaOauth, $ionicPopup) {
+  $scope.usersettings = store.get('userSettings');
+  $scope.askAnswer = store.get('askAnswer');
+  $scope.answerQuestion = function(id) {
+    alert(id);
+    $location.path('answer-question');
+  }
 }]);
---
+++ www/app/controllers/answerQuestionController.js
@@ -0,0 +1,4 @@
+app.controller('answerQuestionController', ['$scope','processApiCallService', 'apiService','$state','store', '$rootScope', 'authService', '$location', '$fancyModal','$sce', '$timeout','$cordovaOauth', '$ionicPopup', '$ionicHistory', function ($scope, processApiCallService, apiService, $state, store, $rootScope, authService, $location, $fancyModal, $sce, $timeout, $cordovaOauth, $ionicPopup, $ionicHistory) {
+	$scope.usersettings = store.get('userSettings');
+
+}]);
